c3fee6f2ec7948f7b25384c26ca9757ee0de7152
piotr <nwg.piotr@gmail.com>
2022-05-21 12:53:05 +0200

    fix setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(f_name):
+    return open(os.path.join(os.path.dirname(__file__), f_name)).read()
+
+
+setup(
+    name='nwg-icon-picker',
+    version='0.0.1',
+    description='GTK icon picker with textual search',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "": ["resources/*"]
+    },
+    url='https://github.com/nwg-piotr/nwg-icon-picker',
+    license='MIT',
+    author='Piotr Miller',
+    author_email='nwg.piotr@gmail.com',
+    python_requires='>=3.6.0',
+    install_requires=[],
+    entry_points={
+        'gui_scripts': [
+            'nwg-icon-picker = nwg_icon_picker.main:main',
+        ]
+    }
+)
