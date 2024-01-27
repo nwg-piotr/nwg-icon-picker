b60e45dea4c9fd4cc23036b950c015e0362bc878
piotr <nwg.piotr@gmail.com>
2024-01-28 00:38:34 +0100

    install README.md, LICENSE

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@
 python3 setup.py install --optimize=1
 cp nwg-icon-picker.svg /usr/share/pixmaps/
 cp nwg-icon-picker.desktop /usr/share/applications/
+
+install -Dm 644 -t "/usr/share/licenses/nwg-icon-picker" LICENSE
+install -Dm 644 -t "/usr/share/doc/nwg-icon-picker" README.md
